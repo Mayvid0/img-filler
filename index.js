895230d3ccc034c25242087de7016e9493f644a5
Mayvid0 <divbillowria@gmail.com>
2024-03-01 03:51:09 +0530

    first

---
+++ index.js
@@ -0,0 +1,80 @@
+/**
+ *
+ * @param {string|number} tag
+ * @param {string|number} height
+ * @param {string|number} width
+ * @returns {string}
+ */
+
+const imagePlaceholderPixabay = async (
+  tag = "nature",
+  height = 100,
+  width = 100
+) => {
+  try {
+    const pixabayApiKey = "42634845-0a63eaaafa679340da05531a3";
+    const response = await fetch(
+      `https://pixabay.com/api/?key=${pixabayApiKey}&q=${tag}&image_type=photo&orientation=horizontal`
+    );
+
+    if (!response.ok) {
+      console.error(`Error: ${response.statusText}`);
+      return "https://images.unsplash.com/photo-1709040567086-ee176caa99f9?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    }
+
+    const data = await response.json();
+
+    if (data.totalHits === 0) {
+      console.error("No images found for the query:", tag);
+      return "https://images.unsplash.com/photo-1709040567086-ee176caa99f9?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    }
+
+    const randomIndex = Math.floor(Math.random() * data.hits.length);
+    const imageUrl = data.hits[randomIndex].largeImageURL;
+    console.log(imageUrl);
+    return imageUrl;
+  } catch (error) {
+    console.error("Error fetching image from Pixabay:", error);
+    return "https://images.unsplash.com/photo-1709040567086-ee176caa99f9?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  }
+};
+
+export const imagePlaceholder = async (
+  tag = "nature",
+  height = "100",
+  width = "100"
+) => {
+  if (
+    typeof tag === "number" &&
+    typeof height === "number" &&
+    typeof width === "string" &&
+    width === "100"
+  ) {
+    width = height.toString();
+    height = tag.toString();
+    tag = "nature";
+  }
+  const unsplashApiKey = "n1iCdjGdOeKYEos7ARWasBwArKIwoDBJH7l4WhPfJTM";
+  try {
+    const unsplashResponse = await fetch(
+      `https://api.unsplash.com/photos/random?query=${tag}&client_id=${unsplashApiKey}&w=${width}&h=${height}`
+    );
+
+    if (!unsplashResponse.ok) {
+      if (unsplashResponse.status === 403) {
+        console.log("Rate limit exceeded, trying Pixabay...");
+        return imagePlaceholderPixabay(tag, height, width);
+      } else {
+        console.error(`Error: ${unsplashResponse.statusText}`);
+        return "https://images.unsplash.com/photo-1709040567086-ee176caa99f9?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+      }
+    }
+
+    const data = await unsplashResponse.json();
+    const imageUrl = data.urls.raw;
+    return imageUrl;
+  } catch (error) {
+    console.error("Error fetching image from Unsplash:", error);
+    return "https://images.unsplash.com/photo-1709040567086-ee176caa99f9?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  }
+};
---
+++ readme.md
@@ -0,0 +1,70 @@
+# img-filler
+
+img-filler is a lightweight npm package that simplifies the process of obtaining image URLs based on a provided prompt or tag. Instead of manually searching and scraping images from different sources, img-filler provides a convenient way to fetch related image URLs that can be used in your components or applications.
+
+## Installation
+
+You can install img-filler via npm or yarn:
+
+```bash
+npm install img-filler
+```
+
+OR
+
+```bash
+yarn add img-filler
+```
+
+### Usage
+
+To use img-filler, import the imgPlaceholder function from the package and call it with the desired prompt or tag. The function will return a URL for a related image.
+
+```javascript
+import { imgPlaceholder } from 'img-filler';
+
+const imageUrl = imgPlaceholder('nature');
+console.log(imageUrl); // Output: URL for a nature-related image
+```
+
+## Example
+
+```js
+import React from 'react';
+import { imgPlaceholder } from 'img-filler';
+
+const ImageComponent = ({ tag }) => {
+    const [imageUrl, setImageUrl] = useState(null);
+
+    useEffect(() => {
+        // Fetch the image URL asynchronously
+        const fetchImageUrl = async () => {
+            try {
+                // Call imagePlaceholder(tag) 
+                const url = await imagePlaceholder(tag);
+                // Set the obtained image URL in state
+                setImageUrl(url);
+            } catch (error) {
+                console.error('Error fetching image URL:', error);
+            }
+        };
+
+        fetchImageUrl(); // Call fetchImageUrl when the component mounts
+    }, []);
+
+     return (
+        <div>
+            {/* Conditional rendering based on imageUrl */}
+            {imageUrl ? (
+                <img src={imageUrl} alt="Fire" />
+            ) : (
+                <p>Loading...</p>
+            )}
+        </div>
+    );
+};
+
+export default ImageComponent;
+```
+
+
